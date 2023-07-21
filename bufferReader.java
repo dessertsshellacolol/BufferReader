4e154af97b2151dd35082fa6e78305126369016b
dessertsshellacolol <138755171+dessertsshellacolol@users.noreply.github.com>
2023-07-21 08:11:38 +0100

    Create bufferReader.java

---
+++ bufferReader.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("example.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
